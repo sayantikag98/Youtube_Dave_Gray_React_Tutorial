a8618c557f2e240996e553f2c7d5fbd69fd1728e
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-09-30 23:45:01 +0530

    Changed Content.js and few styles

--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,60 @@
 import { useState } from "react";
 
 export default function Content() {
-  const [groceryItem, setGroceryItem] = useState("Grocery item");
-  const [count, setCount] = useState(0);
-  const groceries = ["Wheat Flour", "Sugar", "Salt", "Red Chili Powder"];
+  const [groceryItem, setGroceryItem] = useState([
+    {
+      id: 1,
+      pdt: "Toothpaste",
+      checked: false,
+    },
+    {
+      id: 2,
+      pdt: "Cooking Pan",
+      checked: false,
+    },
+    {
+      id: 3,
+      pdt: "Table",
+      checked: false,
+    },
+    {
+      id: 4,
+      pdt: "Fan",
+      checked: false,
+    },
+  ]);
 
-  const HeandleOnClick = () => {
-    setGroceryItem(groceries[Math.floor(Math.random() * 4)]);
-    console.log(count);
-    setCount(count + 1);
-    setCount(count + 1);
+  const HandlerOnChange = (event) => {
+    groceryItem.forEach((ele, ind) => {
+      if (ele.pdt === event.target.dataset.type) {
+        let copyObj = [...groceryItem];
+        copyObj[ind].checked = copyObj[ind].checked === true ? false : true;
+        setGroceryItem(copyObj);
+      }
+    });
   };
 
-  // const HandleOnCLick2 = (name) => {
-  //   console.log(`${name}`);
-  // };
-
-  // const HandleOnCLick3 = (event) => {
-  //   console.log(event.target.dataset.name);
-  //   console.log(event.target.textContent);
-  // };
-
-  // const btnStyle = {
-  //   margin: "20px",
-  // };
+  const AddItem = () =>
+    groceryItem.map((ele) => {
+      return (
+        <li key={ele.id}>
+          <input
+            onChange={HandlerOnChange}
+            type="checkbox"
+            data-type={ele.pdt}
+            checked={ele.checked}
+          />
+          <label style={{ padding: "10px" }}>{ele.pdt}</label>
+          <button>Delete</button>
+        </li>
+      );
+    });
 
   return (
     <main id="main-div">
-      <p>{groceryItem}</p>
-      <button onClick={HeandleOnClick}>Click it to display grocery item</button>
-
-      {/* <button style={btnStyle} onClick={() => HandleOnCLick2("Sayantika")}>
-        Click it to print name
-      </button>
-
-      <button style={btnStyle} onClick={HandleOnCLick3} data-name="click event">
-        Click it to display event
-      </button> */}
+      <ul>
+        <AddItem />
+      </ul>
     </main>
   );
 }
